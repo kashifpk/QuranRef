030a0c9f246b2e519bd079a42136e1384d8e3cc6
Kashif Iftikhar <kashif@compulife.com.pk>
2023-05-28 15:29:49 +0500

    Started using poetry and pyproject.toml for packaging and setup.

---
+++ quranref/logging.py
@@ -0,0 +1,23 @@
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(asctime)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "quranref": {"handlers": ["default"], "level": "DEBUG"},
+        "arango_orm": {"handlers": ["default"], "level": "DEBUG", "qual_name": "arango_orm"},
+    }
+}
